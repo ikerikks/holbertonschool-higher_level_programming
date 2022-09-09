92f213ff26418f4f845a11d8be9deeaf4b1c25a1
ikerikks <ikerikks@gmail.com>
2022-09-09 15:19:58 +0200

    task6: correction of code style

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,10 +3,9 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8:
-           print(f"{i}{j}", end="")
+            print(f"{i}{j}", end="")
 
         else:
             print(f"{i}{j}, ", end="")
 
 print("")
-
