ca1ffbb599e3417e87b1e7801c607da8508f9ba4
ikerikks <ikerikks@gmail.com>
2022-09-09 20:22:25 +0200

    task8: correction of a condition

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        if ord(letter) in range(97, 122):
+        if ord(letter) in range(97, 123):
             letter = chr(ord(letter) - 32)
 
         print("{}".format(letter), end="")
