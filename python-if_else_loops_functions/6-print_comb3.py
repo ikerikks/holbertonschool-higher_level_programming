08e6aa402c9b2eb2120b16664e61d3893b3510ff
ikerikks <ikerikks@gmail.com>
2022-09-09 15:24:19 +0200

    task6: modifing f string format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,9 +3,9 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8:
-            print(f"{i}{j}", end="")
+            print("{}{}".format(i, j), end="")
 
         else:
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i, j), end="")
 
 print("")
