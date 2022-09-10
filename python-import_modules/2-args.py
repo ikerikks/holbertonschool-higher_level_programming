e6c7e1b97e5bfbfaf4230ecd01f5381838035b84
ikerikks <ikerikks@gmail.com>
2022-09-10 20:07:20 +0200

    tsk2: correction of pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     if i < 2:
         print("0 arguments.".format(i))
 
-    else :
-        if (i - 1) < 2: 
+    else:
+        if (i - 1) < 2:
             print("{} argument:".format(i - 1))
-        
+
         else:
             print("{} arguments:".format(i - 1))
 
