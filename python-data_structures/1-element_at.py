a3563752b79507b3f2b50b227d1d1a7499a33ff9
ikerikks <ikerikks@gmail.com>
2022-09-12 12:03:03 +0200

    unction that retrieves an element from a list like in C

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
-        for idx in range(len(my_list)):
-            idx
-        return idx
+        return my_list[idx]
+
