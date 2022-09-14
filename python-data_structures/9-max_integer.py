8727074549b9c5d622f7dbab3ccbf85a576d4080
ikerikks <ikerikks@gmail.com>
2022-09-14 12:33:52 +0200

    function that finds the biggest integer of a lis

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    if my_list is None:
+        return None
+
+    for i in my_list:
+        if i > max:
+            max = i
+
+    return max
