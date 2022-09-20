d6ef9cb3f3c7bb0eb571886938dfafc833995856
ikerikks <ikerikks@gmail.com>
2022-09-20 11:06:15 +0200

    class that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""empty class that defines a square."""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """Square class. """
+
+    def __init__(self, size):
+        """defines a square.
+        Args:
+            size : size of square
+        Returns:
+            None
+        """
+
+        return None
