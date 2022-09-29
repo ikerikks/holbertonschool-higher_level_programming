ca98a0d5a593292ea8f47ed9d2bc67076cc1c980
ikerikks <ikerikks@gmail.com>
2022-09-29 10:29:46 +0200

    function def copy_list(l): that returns a copy of a list

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-""" copy a list"""
-
-
 def copy_list(l):
-    """ function that returns a copy of a list
-    Args:
-        l: list
-    Returns:
-        copy of list
-    """
     new_list = l[:]
     return new_list
