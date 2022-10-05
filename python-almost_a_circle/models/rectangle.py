b04cad178b5f3dd14bd680c914eb1d19f2a91eea
ikerikks <ikerikks@gmail.com>
2022-10-05 10:45:21 +0200

    task2:Write the class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,8 +7,8 @@ class Rectangle(Base):
     """class that inherits from another"""
     def __init__(self, width, height, x=0, y=0, id=None):
 
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
