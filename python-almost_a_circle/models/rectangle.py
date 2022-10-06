4296ace6ce649dee13614fd154b4415cde4c4113
ikerikks <ikerikks@gmail.com>
2022-10-06 16:35:43 +0200

    task2: Write the class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,13 +2,44 @@
 """class inheritance"""
 from models.base import Base
 
-
 class Rectangle(Base):
     """class that inherits from another"""
     def __init__(self, width, height, x=0, y=0, id=None):
-
+        
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise(TypeError("width must be an integer"))
+        if value <= 0:
+            raise(ValueError("width must be > 0"))
+        self.width = value
+    
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise(TypeError("height must be an integer"))
+
+        if value <= 0:
+            raise(ValueError("height must be > 0"))
+        self._height = value
+    
+
+    
