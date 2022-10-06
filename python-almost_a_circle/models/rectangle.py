5217e1b16e76b96e97b8d881c870f8a19c78b541
ikerikks <ikerikks@gmail.com>
2022-10-06 16:50:55 +0200

    task2: Write the class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,10 +2,11 @@
 """class inheritance"""
 from models.base import Base
 
+
 class Rectangle(Base):
     """class that inherits from another"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        
+
         self.__width = width
         self.__height = height
         self.__x = x
@@ -16,7 +17,7 @@ class Rectangle(Base):
     def width(self):
         """width getter"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """width setter"""
@@ -25,12 +26,12 @@ class Rectangle(Base):
         if value <= 0:
             raise(ValueError("width must be > 0"))
         self.width = value
-    
+
     @property
     def height(self):
         """height getter"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """height setter"""
@@ -40,6 +41,31 @@ class Rectangle(Base):
         if value <= 0:
             raise(ValueError("height must be > 0"))
         self._height = value
-    
 
-    
+        @property
+        def x(self):
+            """x getter"""
+            return self.__x
+
+        @x.getter
+        def x(self, value):
+            """x getter"""
+            if type(value) is not int:
+                raise(TypeError("x must be an integer"))
+            if x > 0:
+                raise(ValueError("x must be <= 0"))
+            self.__x = value
+
+        @property
+        def y(self):
+            """y getter"""
+            return self.__y
+
+        @y.getter
+        def y(self, value):
+            """y getter"""
+            if type(value) is not int:
+                raise(TypeError("y must be an integer"))
+            if y > 0:
+                raise(ValueError("y must be <= 0"))
+            self.__y = value
