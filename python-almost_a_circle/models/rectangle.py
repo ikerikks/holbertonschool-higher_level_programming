e28a0004aca60135fbc51ee68f9c618d75f5ff92
ikerikks <ikerikks@gmail.com>
2022-10-07 14:25:32 +0200

    correction of tabs and adding of code

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """class inheritance"""
-import Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -42,30 +42,67 @@ class Rectangle(Base):
             raise(ValueError("height must be > 0"))
         self.__height = value
 
-        @property
-        def x(self):
-            """getter of x"""
-            return self.__x
+    @property
+    def x(self):
+        """getter of x"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """setter of x"""
-            if type(value) is not int:
-                raise(TypeError("x must be an integer"))
-            if value < 0:
-                raise(ValueError("x must be <= 0"))
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """setter of x"""
+        if type(value) is not int:
+            raise(TypeError("x must be an integer"))
+        if value < 0:
+            raise(ValueError("x must be <= 0"))
+        self.__x = value
 
-        @property
-        def y(self):
-            """getter of y"""
-            return self.__y
+    @property
+    def y(self):
+        """getter of y"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """setter of y"""
-            if type(value) is not int:
-                raise(TypeError("y must be an integer"))
-            if value < 0:
-                raise(ValueError("y must be <= 0"))
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """setter of y"""
+        if type(value) is not int:
+            raise(TypeError("y must be an integer"))
+        if value < 0:
+            raise(ValueError("y must be <= 0"))
+        self.__y = value
+
+    def area(self):
+        """Calculate area of rectangle"""
+        return self.__height * self.__width
+
+    def display(self):
+        """Display a rectangle"""
+        rectangle = self.y * "\n"
+        for i in range(self.height):
+            rectangle += (" " * self.x)
+            rectangle += ("#" * self.width) + "\n"
+        print(rectangle, end='')
+
+    def __str__(self):
+        """str special method"""
+        str_rectangle = "[Rectangle] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_rectangle + str_id + str_xy + str_wh
+
+    def update(self, *args, **kwargs):
+        """Update method"""
+        if args is not None and len(args) != 0:
+            list_atr = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns the dictionary representation"""
+        td = {'x': self.x, 'y': self.y, 'id': self.id,
+              'height': self.height, 'width': self.width}
+        return td
