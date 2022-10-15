5a01b96febcad50405e8c5ff3c98fd0a05ace87a
ikerikks <ikerikks@gmail.com>
2022-10-15 13:05:59 +0200

    correction: adding of attributes

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,6 +4,8 @@
 
 class Rectangle:
     """class rectangle"""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initializes new rectangle
         Args:
@@ -12,6 +14,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
