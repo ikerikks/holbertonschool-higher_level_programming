28d583b4b5c9b48a2a5b7d292c11a3aeb0d476e4
ikerikks <ikerikks@gmail.com>
2022-11-07 15:20:51 +0100

    cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    state_id INT FOREIGN KEY REFERENCES states(id),
+    name VARCHAR(256);
+)
