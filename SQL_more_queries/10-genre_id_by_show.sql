a20cd35efb3decd64b8ae93c87170a9b375b212f
ikerikks <ikerikks@gmail.com>
2022-11-07 19:17:41 +0100

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Genre ID by show
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_show_genres.id, tv_show_genres.genre_id;
