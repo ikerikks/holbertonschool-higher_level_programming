fccdd76a94e9bda5dfdecafbb84adfef312432c4
ikerikks <ikerikks@gmail.com>
2022-11-08 12:09:03 +0100

    task11:Genre ID for all shows update of join

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,11 @@
 -- Genre ID for all shows
-UPDATE tv_show_genres
-SET tv_show_genres.genre_id = NULL
-WHERE tv_show_genres.genre_id = NULL;
+-- UPDATE tv_show_genres
+-- SET tv_show_genres.genre_id = NULL
+-- WHERE tv_show_genres.genre_id = NULL;
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id 
+-- OR tv_shows.id IS NULL AND tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
 
