31be2456989fc6289cecded7663fbabd14ee66c6
ikerikks <ikerikks@gmail.com>
2022-11-10 15:27:27 +0100

    sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Filters states """
+
+import sys
+
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        charset="utf8",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * from states WHERE name LIKE BINARY '\"{}\"'\
+        ORDER BY id".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
