6b4a0f1a8d61ed9db508b57176807a6afc4f112a
ikerikks <ikerikks@gmail.com>
2022-11-10 14:25:39 +0100

    Filter states update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY staes.id;")
     rows = cur.fetchall()
     for row in rows:
         if row[1] == "N":
