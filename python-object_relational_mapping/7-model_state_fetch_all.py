ef586e20f7c3cb2ea72df8b285847eaf0672f8a8
ikerikks <ikerikks@gmail.com>
2022-11-10 19:02:41 +0100

    blank line

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}' \
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), \
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
