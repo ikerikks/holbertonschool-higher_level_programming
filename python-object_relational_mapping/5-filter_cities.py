c50c2570cbb4f29fb7f1c97402bc822487078c5a
ikerikks <ikerikks@gmail.com>
2022-11-10 18:51:02 +0100

    all cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -16,9 +16,10 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-        FROM cities JOIN states ON cities.state_id = states.id; \
-            WHERE sates.name = '{}';".format(sys.argv[4]))
-    rows = cur.fetchall()
-    print(", ".join([row[1] for row in rows]))
+        FROM cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    print(", ".join([state[1] for state in states]))
     
 
