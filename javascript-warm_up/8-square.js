a6a7e7ce12874a82490e02acd4cdb223cc7e9e89
ikerikks <ikerikks@gmail.com>
2022-12-08 13:14:03 +0100

    task8: print a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const firstArg = Number(process.argv[2]);
+let i = 0;
+let j = 0;
+let squareSymbol = '';
+
+if (!firstArg) {
+  console.log('Missing size');
+} else {
+  while (i < firstArg) {
+    while (j < firstArg) {
+      squareSymbol += '#';
+      j++;
+    }
+
+    console.log(squareSymbol);
+    i++;
+  }
+}
