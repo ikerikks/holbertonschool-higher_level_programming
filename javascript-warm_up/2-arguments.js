1fb1cda9089e4a5357611756596274f8dcb34353
ikerikks <ikerikks@gmail.com>
2022-12-09 12:59:58 +0100

    task2:correction of statement

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
+const length = process.argv.length;
+
+if (length === 3) {
   console.log('Argument found');
+} else if (length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
