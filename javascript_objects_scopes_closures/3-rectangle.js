fef23e4c5a4a3bc3f2942886e139df2f8af9d594
ikerikks <ikerikks@gmail.com>
2022-12-12 16:01:44 +0100

    rectangle #3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,15 +9,14 @@ class Rectangle {
   }
 
   print () {
-    let j = 0;
-    let i = 0;
     let row = '';
 
-    for (; i < this.height; i++) {
-      for (; j < this.width; j++) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
         row += 'X';
       }
       console.log(row);
+      row = '';
     }
   }
 }
