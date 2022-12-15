8da48b772c5cd5f2d8e32c31e1b4e949be0dec81
ikerikks <ikerikks@gmail.com>
2022-12-15 14:27:07 +0100

    updating the loop

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
+// const file = require('./file_2');
 const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
-let counter = 0;
-let i = 0;
-let j = 0;
 
 request(apiUrl, (error, response, body) => {
   if (error === null) {
+    let counter = 0;
     body = JSON.parse(body);
     const results = body.results;
 
-    for (i in results) {
-      for (j in results[i].characters) {
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].characters.length; j++) {
         if (results[i].characters[j] === characterUrl) {
           counter++;
         }
