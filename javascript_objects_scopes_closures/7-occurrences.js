0062c14940e42c1474cbd4045c3de82f408956b2
ikerikks <ikerikks@gmail.com>
2022-12-16 16:38:40 +0100

    number of occurences in a list

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+
+  list.forEach((element) => {
+    if (element === searchElement) {
+      counter++;
+    }
+  });
+  return counter;
+};
