0c4a9a9163313816e98df18422c4150848453785
ikerikks <ikerikks@gmail.com>
2022-12-16 19:13:55 +0100

    update

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,10 @@ const request = require('request');
 const apiUrl = process.argv[2];
 // const file = require('./file_2');
 const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
-
+let counter = 0;
 request(apiUrl, (error, response, body) => {
   if (error === null) {
-    let counter = 0;
+    
     let i = 0;
     let j = 0;
     body = JSON.parse(body);
