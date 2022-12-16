bd3e405717ce246db8921bef003dc66e00bdd50c
ikerikks <ikerikks@gmail.com>
2022-12-16 20:14:43 +0100

    number of task completed by user id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -6,34 +6,18 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (error === null) {
     let i = 0;
-    let sum = 0;
-    let counter = 1;
     const todosTab = {};
-    let setValue = 0;
 
-    // conversion of json data into js objects
     body = JSON.parse(body);
-    // setting the control value for userId
-    setValue = body[0].userId;
 
     for (i in body) {
-      // check if the current userId is the same
-      if (body[i].userId > setValue) {
-        setValue = body[i].userId;
-        // results.push(sum);
-        todosTab[counter] = sum;
-        sum = 0;
-        counter++;
+      if (body[i].completed === true) {
+        if (todosTab[body[i].userId] === undefined) {
+          todosTab[body[i].userId] = 0;
+        } else {
+          todosTab[body[i].userId]++;
+        }
       }
-      // computing number of task complete
-      if (body[i].completed) {
-        sum++;
-      }
-    }
-    // adding number of task complete of last userId
-    if (body[body.length - 1]) {
-      todosTab[counter] = sum;
-      counter++;
     }
 
     console.log(todosTab);
